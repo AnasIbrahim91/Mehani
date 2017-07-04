8ee7cc038800c6804b596ab02fc992855a7e437c
AnasIbrahim92 <anas.abuhilal.1992@gmail.com>
2017-07-04 14:41:36 +0300

    Add new

--- app/src/main/java/com/wyanbu/mehani/mehani/MobileRegister.java
+++ app/src/main/java/com/wyanbu/mehani/mehani/MobileRegister.java
@@ -1,15 +1,20 @@
 package com.wyanbu.mehani.mehani;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MobileRegister extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (true) {
+            setContentView(R.layout.activity_mobile_register);
+        } else {
+            setContentView(R.layout.activity_mobile_register2);
 
-        setContentView(R.layout.activity_mobile_register);
+        }
+
 
     }
 }
